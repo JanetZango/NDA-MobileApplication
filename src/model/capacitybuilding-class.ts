fa3fc9928c334d42968f93ab24c6dd0d9c14414f
bmmaphophe <bmmaphophe@gmail.com>
2020-02-22 16:35:48 +0200

    updated the model folder and class

---
+++ src/model/capacitybuilding-class.ts
@@ -0,0 +1,7 @@
+export class CapacityBuilding {
+
+    nameOfCso: string;
+    
+
+
+}
--- src/providers/api/api.ts
+++ src/providers/api/api.ts
@@ -1,5 +1,8 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
+import { catchError, tap } from 'rxjs/operators';
 
 /*
   Generated class for the ApiProvider provider.
@@ -7,6 +10,11 @@ import { Injectable } from '@angular/core';
   See https://angular.io/guide/dependency-injection for more info on providers
   and Angular DI.
 */
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
 @Injectable()
 export class ApiProvider {
 
@@ -14,4 +22,39 @@ export class ApiProvider {
     console.log('Hello ApiProvider Provider');
   }
 
+  private url:string;
+
+
+  public getUser( email:string){
+    const url = `${this.url}/getUser`;
+    return this.http.post(url,{user:email},httpOptions);
+
+  }
+
+  public verifyOpt(code:any){
+    const url = `${this.url}/verifyOpt`;
+    return this.http.post(url,{user:code},httpOptions);
+  }
+
+
+    /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * 
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.log(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      console.error(`Api: ${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
 }
