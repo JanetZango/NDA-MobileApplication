786f1bc8590237f6708f1bc47a4324444eb7d859
Janet Zango <janetzango@gmail.com>
2020-02-22 17:03:32 +0200

    models added

---
+++ src/model/assessment-class.ts
@@ -0,0 +1,7 @@
+export class Assessment {
+    CsoName:string;
+    assessment_date:Date;
+    assessment_type_id:number;
+    assessmentQ:string;
+
+}
--- src/model/capacitybuilding-class.ts
+++ src/model/capacitybuilding-class.ts
@@ -1,7 +1,15 @@
 export class CapacityBuilding {
-
     nameOfCso: string;
-    
-
+    capacity_building_type_id:number; 
+    province_id:number;
+    district_id:number;
+    municipality_id:number;
+    partner_id:number;
+    venue:string;
+    facilitator_name:string;
+    Funding_source_id:number;
+    CollectedBy:number;
+    start_date:Date;
+    end_date:Date;
 
 }
