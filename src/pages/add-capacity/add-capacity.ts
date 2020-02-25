2bea3fb38c5093c19dd12760da15aa47879b25bc
bmmaphophe <bmmaphophe@gmail.com>
2020-02-25 05:43:57 +0200

    updated the data picker and created the filters for select on the add capacity page

--- src/pages/add-capacity/add-capacity.ts
+++ src/pages/add-capacity/add-capacity.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { IonicPage, NavController, NavParams, AlertController, LoadingController } from 'ionic-angular';
 import { NgModel, NgForm } from '@angular/forms';
 import { DisplayListOfCapacityPage } from '../display-list-of-capacity/display-list-of-capacity';
 import { LookUpService } from '../../providers/lookup/lookups.service';
@@ -20,8 +20,10 @@ import { DataProvider } from '../../providers/dataproviders/dataprovider';
   selector: 'page-add-capacity',
   templateUrl: 'add-capacity.html',
 })
-export class AddCapacityPage {
 
+
+
+export class AddCapacityPage {
 
 //arrays
   capacityArr = new Array();
@@ -29,6 +31,11 @@ export class AddCapacityPage {
   provinceArr = new Array();
   municipalityArr = new Array();
   partnerArr = new Array();
+
+  // arrays that store data
+  districtArrData = new Array();
+  municipalityArrData = new Array();
+
   capacity: CapacityBuilding = new CapacityBuilding();
   constructor(
     public navCtrl: NavController, 
@@ -36,9 +43,13 @@ export class AddCapacityPage {
     public lookupService: LookUpService,
     public entityProvider: EntityProvider,
     public alertCtrl: AlertController,
-    public csoProvider: DataProvider
+    public csoProvider: DataProvider,
+    public loadingController: LoadingController 
     ) {
+      
   }
+
+  
 
   ionViewDidLoad() {
     //console.log('ionViewDidLoad AddCapacityPage');
@@ -53,41 +64,80 @@ export class AddCapacityPage {
     this.navCtrl.push(DisplayListOfCapacityPage)
   }
 
+  /**
+   * Get Capacity Building
+   */
   getCapacityBuilding(){
     this.lookupService.getCapacityBuildingType().subscribe(res =>{
       this.capacityArr = res;
     })
   }
 
+  /**
+   * Get District
+   */
   getDistrict(){
     this.lookupService.getDistrict().subscribe(res =>{
-      this.districtArr = res;
+      this.districtArrData = res;
     })
   }
 
+  /**
+   *  Get Province
+   */
   getProvince(){
     this.lookupService.getProvince().subscribe(res =>{
       this.provinceArr = res;
     })
   }
-
+  /**
+   * Get Partner Type
+   */
   getPartnerType(){
     this.lookupService.getPartnerType().subscribe(res =>{
       this.partnerArr = res;
     })
   }
 
+  /**
+   * Get Municipality
+   */
   getMunicipality(){
     this.lookupService.getLocalMunicipality().subscribe(res =>{
-      this.municipalityArr = res;
+      this.municipalityArrData = res;
     })
   }
 
+  /**
+   * to populate the district by select provice id 
+   * @param proviceId
+   */
+  populateDistrict(proviceId: NgModel){
+    this.districtArr =  this.districtArrData
+      .filter(x => x.province_id === proviceId);
+  }
+
+  /**
+   * to populate the Municipality by select district id
+   * @param districtId
+   */
+  populateMunicipality(districtId: NgModel){
+    debugger
+    this.municipalityArr = this.municipalityArrData
+      .filter(m => m.district_id === districtId);
+  }
+
+  /**
+   * Saving Capacity Building
+   * 
+   * @param capacity 
+   */
   addCapacity(capacity: NgForm){
-    this.entityProvider.saveCapacityBuilding(capacity.value)
+   
+      this.entityProvider.saveCapacityBuilding(capacity.value)
       .subscribe(res =>{
         if(res.status === 201){
-          // TODO
+          capacity.reset();
         }else{
           const alert = this.alertCtrl.create({
             title: 'Alert',
