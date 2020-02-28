e3910efec495c5c90b311b55133cd2790a0bf141
Janet Zango <janetzango@gmail.com>
2020-02-28 07:34:03 +0200

    chnages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,7 +16,7 @@ import { LoginPage } from '../pages/login/login';
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = LoginPage;
+  rootPage:any = HomePage;
   // rootPage:any = HomePloage;
   showSplash = true;
   constructor(platform: Platform, 
