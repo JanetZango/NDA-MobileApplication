f4b90c56a59e3ebf1c7016435420e148e0e7014b
Mfana Conco <pegasus@Mfanas-MacBook-Pro.local>
2020-03-16 11:16:27 +0200

    added capacity building

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -10,8 +10,8 @@ export const environment = {
     isDebugMode: true,
     dismissLoader: 2000,
     //apiUrl: 'http://157.245.251.223:5000/' Test url,
-    //apiUrl: 'http://134.122.96.252:5000',
-    apiUrl: 'http://127.0.0.1:5000',
+    apiUrl: 'http://134.122.96.252:5000',
+    //apiUrl: 'http://127.0.0.1:5000',
     buildVesion: '0.0.1',
     apiError: 'There was an error processing your request, please try again later.'
 };
--- src/pages/add-capacity/add-capacity.ts
+++ src/pages/add-capacity/add-capacity.ts
@@ -1,19 +1,15 @@
 import {Component, OnInit} from '@angular/core';
 import { IonicPage, NavController, NavParams, AlertController, LoadingController } from 'ionic-angular';
-import { NgModel, NgForm,Validators, FormBuilder, FormGroup } from '@angular/forms';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
 import { LookUpService } from '../../providers/lookup/lookups.service';
-import { CapacityBuilding } from '../../model/capacitybuilding.model';
-import { EntityProvider } from '../../providers/entity/cso';
-import { DataProvider } from '../../providers/dataproviders/dataprovider';
-import {CSoResponseData, DisplayListOfCsoPage} from "../display-list-of-cso/display-list-of-cso";
 import {CapacityBuildingPayload} from "../../model/payload/capacitybuildingpayload.model";
 import {DisplayListOfCapacityPage} from "../display-list-of-capacity/display-list-of-capacity";
 import {LandingPage} from "../landing/landing";
 import {LoginPage} from "../login/login";
 import {Storage} from "@ionic/storage";
 import {CsoService} from "../../service/cso.service";
-import {Cso} from "../../model/cso.model";
 import {CapacityBuildingService} from "../../service/capacity-building.service";
+import {UserService} from "../../service/user.service";
 
 @IonicPage()
 @Component({
@@ -43,11 +39,7 @@ export class AddCapacityPage implements OnInit{
   capacityBuildingPayload: CapacityBuildingPayload;
   private capacityBuildingForm: FormGroup;
 
-  originalListOfCsoes: Cso[] = [];
-  filteredListOfCsoes: Cso[] = [];
 
-  showCsoList = true;
-  cso_guid: string;
 
   constructor(
     public navCtrl: NavController,
@@ -58,19 +50,18 @@ export class AddCapacityPage implements OnInit{
     public storage: Storage,
     public alertCtrl: AlertController,
     private formBuilder: FormBuilder,
-    public csoService: CsoService,
+    public userService: UserService
   ) {}
 
   ngOnInit(): void {
     this._getCapacityBuildingType();
     this._getPartnerType();
     this._getPartner();
-    this._getDistrict();
+    this._getDistrict(this.userService.province_guid);
     this._getFundingSource();
     this._getMunicipality();
     this._getProvince();
     this._buildForm();
-    this._getListOfCsoes();
   }
 
   _buildForm(){
@@ -81,7 +72,6 @@ export class AddCapacityPage implements OnInit{
       'venue': ['', [
         Validators.required, Validators.minLength(2), Validators.maxLength(50),]],
       'co_facilitator_name': ['', [Validators.minLength(2), Validators.maxLength(50),]],
-      'province': ['', [Validators.required]],
       'district': ['', [Validators.required]],
       'municipality': ['', [Validators.required]],
       'funding_source': ['', [Validators.required]],
@@ -126,10 +116,11 @@ export class AddCapacityPage implements OnInit{
     })
   }
 
-  _getDistrict() {
+  _getDistrict(province_guid: string) {
     this.lookupService.getDistrict().subscribe(res => {
-      this.listOriginalLookupDistrict = res;
-      this.listFilteredLookupDistrict = res;
+      const listOfDistricts = res;
+      this.listOriginalLookupDistrict = listOfDistricts.filter(x => x.province_guid === province_guid);
+      this.listFilteredLookupDistrict = listOfDistricts.filter(x => x.province_guid === province_guid);
     })
   }
 
@@ -191,7 +182,6 @@ export class AddCapacityPage implements OnInit{
     this.capacityBuildingPayload.venue = this.capacityBuildingForm.value.venue;
     this.capacityBuildingPayload.start_date = this.capacityBuildingForm.value.start_date;
     this.capacityBuildingPayload.end_date = this.capacityBuildingForm.value.end_date;
-    this.capacityBuildingPayload.cso_guid = this.cso_guid;
 
     const _loader = this.loadingCtrl.create({
       content: "Please wait whilst we create capacity...",
@@ -227,30 +217,6 @@ export class AddCapacityPage implements OnInit{
     });
   }
 
-  _getListOfCsoes() {
-
-    const _loader = this.loadingCtrl.create({
-      content: "Please wait information is still loading...",
-      duration: 300000000
-    });
-
-    _loader.present();
-
-    this.csoService.list().subscribe((_responseData:CSoResponseData) => {
-      this.originalListOfCsoes = _responseData.csoes;
-      this.filteredListOfCsoes = _responseData.csoes;
-      _loader.dismiss();
-    },_error => {
-      _loader.dismiss();
-      const alert = this.alertCtrl.create({
-        title: 'Oops',
-        subTitle: 'Something went wrong, please contact administrator.',
-        buttons: ['OK']
-      });
-      alert.present();
-    });
-  }
-
   goBackToHomePage() {
     this.navCtrl.push(LandingPage)
   }
@@ -279,21 +245,4 @@ export class AddCapacityPage implements OnInit{
     });
     alert.present();
   }
-
-  selectCSO(cso:Cso){
-    this.cso_guid = cso.guid;
-    this.showCsoList = false;
-  }
-
-  searchForCso(element: any) {
-    const _needle = element.target.value;
-    if (_needle === '') {
-      this.filteredListOfCsoes = this.originalListOfCsoes;
-      return;
-    }
-    this.filteredListOfCsoes = this.originalListOfCsoes.filter((cso) => {
-      return (cso.name_of_cso.toLowerCase().indexOf(_needle.toLowerCase()) > -1);
-    })
-  }
-
 }
